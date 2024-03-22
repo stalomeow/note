da2a60a3cee9090f22bdaee32cd7cca8f4892357
stalomeow <stalowork@163.com>
2024-03-22 20:41:44 +0800

    Update

--- docs/assets/javascripts/sponsor.js
+++ docs/assets/javascripts/sponsor.js
@@ -1,7 +1,7 @@
 document$.subscribe(() => {
-    const html = `<p style="text-align: center;"><a class="md-button" href="/#_4">赞助</a></p>`;
+    const html = `<hr><p style="text-align: center;"><a class="md-button" href="/#_4">赞助</a></p>`;
 
-    if (window.location.pathname == '/') {
+    if (window.location.hostname === "127.0.0.1" || window.location.pathname === '/') {
         return;
     }
 
