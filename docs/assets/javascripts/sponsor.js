99e21a69c1db3ed398d05bd6471d4239191b70fd
stalomeow <stalowork@163.com>
2024-04-25 18:27:09 +0800

    Remove some plugins

--- docs/assets/javascripts/sponsor.js
+++ docs/assets/javascripts/sponsor.js
@@ -1,9 +1,5 @@
 document$.subscribe(() => {
-    const html = `<br><p style="text-align: center;"><a class="md-button" href="https://stalomeow.com/#coffee" target="_blank">赞助</a></p>`;
-
-    if (window.location.hostname === "127.0.0.1") {
-        return;
-    }
+    const html = `<br><p style="text-align: center;"><a class="md-button" href="https://stalomeow.com/#coffee" target="_blank">赞助</a></p><br>`;
 
     const comments = document.querySelector('#__comments');
     if (comments) {
