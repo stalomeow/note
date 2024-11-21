88cebecc0731d83211e71d6f678b84c4beb23625
stalomeow <stalowork@163.com>
2024-11-21 15:33:17 +0800

    Update copyright

---
+++ scripts/copyright.py
@@ -0,0 +1,19 @@
+import datetime
+import inspect
+import logging
+
+from mkdocs.config.defaults import MkDocsConfig
+
+log = logging.getLogger('mkdocs.plugins')
+
+def get_copyright() -> str:
+    tz = datetime.timezone(datetime.timedelta(hours=8))
+    now = datetime.datetime.now(tz)
+    return inspect.cleandoc(f'''
+        Copyright &copy; 2022-{now.strftime('%Y')} Stalo. All rights reserved. Built on {now.strftime('%Y-%m-%d (UTC+8)')}.<br>
+        Contents are licensed under the <u><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener">CC-BY-NC-SA 4.0</a></u> license, except when otherwise noted.
+        ''')
+
+def on_config(config: MkDocsConfig):
+    config.copyright = get_copyright()
+    return config
